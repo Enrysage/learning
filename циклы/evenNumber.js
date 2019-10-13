c5290c927302122a412a2870ff509178effc19e9
Enrysage <sergiencko.tosha2012@yandex.ru>
2019-10-13 22:23:31 +0300

    begin? MUAHAHAHAH

--- циклы/evenNumber.js
+++ циклы/evenNumber.js
@@ -8,7 +8,7 @@ function createCounter() {
 
 var counter1 = createCounter();
 var counter2 = createCounter();
-console.log(counter1);
+console.log(counter1);    
 console.log(counter2);
 console.log(counter1() + counter2() + counter1());
 console.log(+counter1)
